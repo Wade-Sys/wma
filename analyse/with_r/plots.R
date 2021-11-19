2807f5fce8686a6020ad5398696d5899600bb8c2
Wade-Sys <pauliuz@web.de>
2021-11-19 15:59:02 +0100

    Generierung der Plots

---
+++ analyse/with_r/plots.R
@@ -0,0 +1,13 @@
+ggplot(data = df_wetter_3, aes(x=TMP_MEAN_RND)) + geom_histogram(binwidth = 1, color="white", fill="skyblue") + labs(x="Temperatur (°C)", y="Häufigkeit (abs)", title = "Durchschn. Temperatur (ger.)") + scale_y_continuous(breaks = seq(0,11,1)) + scale_x_continuous(breaks = seq(0,25,1))
+ggplot(data = df_wetter_3, aes(x=TMP_MEAN_RND)) + geom_histogram(binwidth = 2, color="white", fill="skyblue") + labs(x="Temperatur (°C)", y="Häufigkeit (abs)", title = "Durchschn. Temperatur (ger.)") + scale_y_continuous(breaks = seq(0,13,1)) + scale_x_continuous(breaks = seq(0,25,1))
+
+ggplot(data = df_wetter_3, aes(x=DEW_MEAN_RND)) + geom_histogram(binwidth = 1, color="white", fill="skyblue") + labs(x="Temperatur (°C)", y="Häufigkeit (abs)", title = "Durchschn. Taupunkt (ger.)") + scale_y_continuous(breaks = seq(0,13,1)) + scale_x_continuous(breaks = seq(-15,25,1))
+ggplot(data = df_wetter_3, aes(x=DEW_MEAN_RND)) + geom_histogram(binwidth = 3, color="white", fill="skyblue") + labs(x="Temperatur (°C)", y="Häufigkeit (abs)", title = "Durchschn. Taupunkt (ger.)") + scale_y_continuous(breaks = seq(0,13,1)) + scale_x_continuous(breaks = seq(-15,25,1))
+
+ggplot(data = df_wetter_3, aes(x=WND_SR_MEAN_RND)) + geom_histogram(binwidth = 1, color="white", fill="skyblue") + labs(x="Windstärke (m/s)", y="Häufigkeit (abs)", title = "Durchschn. Windstärke (ger.)") + scale_y_continuous(breaks = seq(0,30,2)) + scale_x_continuous(breaks = seq(0,15,1))
+
+ggplot(data = subset(df_wma_2, (Geschlecht=='M')), aes(x=S_KM_FN)) + geom_histogram(binwidth = 50, color="white", fill="skyblue") + labs(x="Laufzeiten (in sek.)", y="Häufigkeit (abs)", title = "Laufzeiten (Männer)") + scale_x_continuous(breaks = seq(7000,9000,100)) + scale_y_continuous(breaks = seq(0,70,5))
+ggplot(data = subset(df_wma_2, (Geschlecht=='W')), aes(x=S_KM_FN)) + geom_histogram(binwidth = 50, color="white", fill="skyblue") + labs(x="Laufzeiten (in sek.)", y="Häufigkeit (abs)", title = "Laufzeiten (Frauen)") + scale_x_continuous(breaks = seq(7000,11000,500)) + scale_y_continuous(breaks = seq(0,70,5))
+ggplot(df_wma_2, aes(x=S_KM_FN,color=Geschlecht, fill=Geschlecht)) + geom_histogram(binwidth = 70,alpha=0.5, position = "dodge") + labs(x="Laufzeiten (in sek.)", y="Häufigkeit (abs)", title = "Laufzeiten (Frauen & Männer)") + scale_x_continuous(breaks = seq(7000,11000,500)) + scale_y_continuous(breaks = seq(0,100,10))
+
+ggsave(filename = "test.png", plot = last_plot(),units = "px",scale = 1, limitsize = FALSE, device = "png")
